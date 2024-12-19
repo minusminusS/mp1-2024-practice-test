88954ef7fac7f11a8f501c69af9d98782809a320
minusminusS <serjsw113322@gmail.com>
2024-12-19 09:33:15 +0300

    Update task2.c

--- 3824Б1ПР5/20 Свистухин Сергей Сергеевич/task2.c
+++ 3824Б1ПР5/20 Свистухин Сергей Сергеевич/task2.c
@@ -4,5 +4,15 @@
 // Найти число серий длины 3 в массиве.
 int task2(unsigned A[], size_t n)
 {
-  return -1;
+    int ans = 0;
+    int ser = 1;
+    for (int i = 1; i < n;i++) {
+        if (A[i] == A[i-1])ser++;
+        else {
+            if (ser == 3)ans++;
+            ser = 1;
+        }
+    }
+    if (ser == 3)ans++;
+    return ans;
 }
